438276a5dc733ba598fc33cdbd9e4855b6a9713d
kami-sa <kamisa1999@yandex.ru>
2019-10-17 10:40:56 +0300

    Add 9.2 task

---
+++ 9.2.py
@@ -0,0 +1,19 @@
+num_of_list = int(input())
+flag = num_of_list
+pupil_list = set()
+while num_of_list != 0:
+    num_of_pup = int(input())
+    if num_of_list == flag:
+        for i in range(num_of_pup):
+            pupil_list.add(input())
+    else:
+        pupils = set()
+        #pupils.add(input() for i in range(int(input())))
+        pupils_count = int(input())
+        for i in range(pupils_count):
+            pupils.add(input())
+        pupil_list = pupil_list.intersection(pupils)
+    num_of_list -= 1
+print(pupil_list)
+
+
