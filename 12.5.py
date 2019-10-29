5cfb4416199cd2244313256653173d808ced4e87
kami-sa <kamisa1999@yandex.ru>
2019-10-30 00:20:51 +0300

    Task 12.6 work bad

--- 12.5.py
+++ 12.5.py
@@ -1,9 +1,45 @@
-str = input()
-count = 1
-for i in range(len(str)-1):
-    if str[i] == str[i+1]:
-        count += 1
-    else:
-        print(count, ':',str[i])
-        count = 1
-print(count, ':',str[-1])
+n = int(input())
+y = 1/n
+a = str(y)
+a = a.lstrip('0')
+a = a.lstrip('.')
+string = ''
+symbol = ''
+num_set = set(a)
+count = len(num_set)
+#print(num_set)
+for i in range(count):
+    if a[i] in num_set:
+        string += a[i];
+        num_set.remove(a[i])
+
+print(string)
+# count = 0
+# for i in range(len(a)-1):
+#     if a[i] != a[i+1]:
+#         string += a[i] #+ a[i+1]
+#     else:
+#         string = a[i]
+#     for j in range(i, len(a), len(string)):
+#         if string == a[i:len(string):1] and string != '0':
+#             count += 1
+#     if count > 1:
+#         print(string)
+#         break;
+    # symbol = a[i]
+    # if symbol != a[i+1]:
+    #     string += symbol
+    # else:
+    #     string = symbol;
+    # if a.count(string) > 1:
+    #     break
+#print(string)
+# n = int(input())
+# r = 1
+# rr = []
+# dd = []
+# while r not in rr:
+#     rr.append(r)
+#     dd.append(10*r//n)
+#     r = 10*r%n
+# print(*dd[rr.index(r):])
