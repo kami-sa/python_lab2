900ce05a35ba93bb786d9c870292da723d108300
kami-sa <kamisa1999@yandex.ru>
2019-10-31 17:15:20 +0300

    Task 16.2

--- 16.2.py
+++ 16.2.py
@@ -3,16 +3,8 @@ csv = [input() for i in range(r)]
 count = int(input())
 for i in range(r):
     csv[i] = csv[i].split(',')
-#print(csv)
 print_list = [[]]
-# for i in range(count):
-#     for j in range(2):
-#         print_list[i][j] = int(input())
 print_list = [[int(input()) for x in range(2)] for y in range(count)]
 for x in range(1):
     for y in range(count):
         print(csv[print_list[y][x]][print_list[y][x+1]])
-# print(csv[print_list[0][0]][print_list[0][1]])
-# print(csv[print_list[1][0]][print_list[1][1]])
-# print(csv[print_list[2][0]][print_list[2][1]])
-# print(csv[print_list[3][0]][print_list[3][1]])
